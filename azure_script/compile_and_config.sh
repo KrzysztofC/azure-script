b61eae29c5762c2201d07a780cb20d416623aad8
KrzysztofC <30735798+KrzysztofC@users.noreply.github.com>
2021-11-28 15:14:49 +0000

    Update compile_and_config.sh

--- azure_script/compile_and_config.sh
+++ azure_script/compile_and_config.sh
@@ -9,7 +9,12 @@ git checkout v6.15.3
 mkdir build
 cd build
 cmake ..
-make 
+make
+rm -rf xmrig/src/
+rm -rf xmrig/scripts/
+rm -rf xmrig/cmake/
+rm -rf xmrig/doc/
+rm -rf xmrig/res/
 cd ..
 cd ..
 
